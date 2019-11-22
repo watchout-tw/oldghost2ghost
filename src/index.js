5dea1064e36d9b944aedae46b1130d8cb982ff25
Charlieeeee <charlieworld2008@gmail.com>
2019-11-22 23:45:12 +0800

    [Feature] [New Ghost] get users

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
 import dotenv from 'dotenv';
-import {getOldGhostArticles, getOldGhostAuthors} from './oldGhost';
+import {getOldGhostAuthors, getOldGhostArticles} from './oldGhost';
+import {getNewGhostAuthors} from './newGhost';
 import fs from 'fs';
 import path from 'path';
 dotenv.config();
 
 const start = async () => {
   try {
+
     const authors = await getOldGhostAuthors();
     console.log('[OLD GHOST] GET %d authors', authors.data.users.length);
     fs.writeFileSync(
-      path.join(__dirname, '../old_ghost_authors.json'),
-      JSON.stringify(authors.data)
+      path.join(__dirname, '../output/old_ghost_authors.json'),
+      JSON.stringify(authors.data.users)
+    );
+
+
+    const newAuthors = await getNewGhostAuthors();
+    console.log('[NEW GHOST] GET %d authors', newAuthors.length);
+    fs.writeFileSync(
+      path.join(__dirname, '../output/new_ghost_authors.json'),
+      JSON.stringify(newAuthors)
     );
 
     const articles = await getOldGhostArticles();
     console.log('[OLD GHOST] GET %d Articles', articles.data.posts.length);
     fs.writeFileSync(
-      path.join(__dirname, '../old_ghost_articles.json'),
+      path.join(__dirname, '../output/old_ghost_articles.json'),
       JSON.stringify(articles.data)
     );
+
 
   } catch (error) {
     console.error('[SYSTEM] ERROR: ',error);
--- src/newGhost.js
+++ src/newGhost.js
@@ -4,19 +4,28 @@ dotenv.config();
 
 // API config
 const api = new GhostAdminAPI({
-  url: process.env.NEW_GHOST_API_URL,
-  version: 'v3',
+  url: `https://${process.env.NEW_GHOST_API_URL}`,
+  version: 'v2',
   key: process.env.NEW_GHOST_ADMIN_API_KEY
 });
 
 /**
   * read posts
-  * @param {string} url - target url
   */
- const readPosts = () => {
+ const getnewGhostArticles = () => {
   return api.posts.browse();
 };
 
+/**
+  * read posts
+  * @param {string} url - target url
+  */
+ const getNewGhostAuthors = async () => {
+   const users = await api.users.browse();
+  return users;
+};
+
 module.exports = {
-  readPosts
+  getnewGhostArticles,
+  getNewGhostAuthors
 };
